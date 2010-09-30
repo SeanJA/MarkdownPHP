0609b0e47cc6fe8fb6742def220707f1c4efb370
SeanJA <sean.sandy@gmail.com>
2010-09-30 16:43:30 -0300

    no need for the license text at the top, it is in License.md

--- classes/markdown.php
+++ classes/markdown.php
@@ -1,15 +1,7 @@
 <?php
 
 /**
- * @copyright (c) 2004-2009 Michel Fortin
- * @copyright (c) 2004-2006 John Gruber
- * @license GPLv2
- * Markdown is free software, available under the terms of the BSD-style open source license reproduced below, or, at your option, under the GNU General Public License version 2 or a later version. If you like this software and want to say thanks, you can contribute my making a donation.
- * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
- * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
- * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
- * Neither the name “PHP Markdown” nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
- * This software is provided by the copyright holders and contributors “as is” and any express or implied warranties, including, but not limited to, the implied warranties of merchantability and fitness for a particular purpose are disclaimed. In no event shall the copyright owner or contributors be liable for any direct, indirect, incidental, special, exemplary, or consequential damages (including, but not limited to, procurement of substitute goods or services; loss of use, data, or profits; or business interruption) however caused and on any theory of liability, whether in contract, strict liability, or tort (including negligence or otherwise) arising in any way out of the use of this software, even if advised of the possibility of such damage.
+ * @license see License.md
  */
 /**
  * The version of markdown in use
